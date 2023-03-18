3b2d45808bafe56fffe6040e7a4129108c4900ac
Till Taeubrich <till.taeubrich@gmail.com>
2023-03-18 15:34:12 +0800

    finish exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (listWithOneBlog) => {
+	const likes = listWithOneBlog[0].likes;
+
+	return likes
+}
+
+const totalLikesAllBlogs = (listWithBlogs) => {
+	const allLikes = listWithBlogs.reduce((accumulator, currentValue) => {
+		return currentValue.likes + accumulator
+	}, 0);
+
+	return allLikes
+}
+
 module.exports = {
-    dummy
+    dummy,
+		totalLikes,
+		totalLikesAllBlogs
 }
