64d18b474c882384c8da81e683348e089a19b481
Till Taeubrich <till.taeubrich@gmail.com>
2023-04-13 13:15:43 +0800

    setup /api/users endpoint

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
 
 mongoose.connect(config.mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
