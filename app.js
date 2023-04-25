300ee45a7a9d55f4f6de6b7b9be963d460d4f372
Till Taeubrich <till.taeubrich@gmail.com>
2023-04-25 17:52:37 +0800

    finish exercise 4.18

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 
+const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
@@ -15,6 +16,7 @@ mongoose.connect(config.mongoUrl)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
